e427b9f7861a7a5adf375e827b55a774bafb3ff1
jspenc72 <jspenc72@gmail.com>
2016-04-22 23:17:03 -0600

    Add example credentials folder.

---
+++ examples/credentials.example.js
@@ -0,0 +1,8 @@
+module.exports = function () {
+	'use strict';
+	var credentials = {
+		username: 'XXXX', 
+		api_key: 'XXXX'
+	};
+	return credentials
+}
---
+++ examples/sorting-example.js
@@ -0,0 +1,12 @@
+var credentials = require('credentials.js')
+var humble = require('../index.js')(credentials)
+//Get list of albums
+humble.albums.get()
+.then(function(res){
+	console.log('albums().get then()')
+	console.log(res.Albums.length)
+})
+.catch(function(error){
+	console.log("Caught the error")
+	console.error(error);
+});
